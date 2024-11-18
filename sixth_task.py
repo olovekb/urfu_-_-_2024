a5c64dd8b7bc745883c4a1094d6ab711e3614cf8
Ankudinov Oleg <97741453+olovekb@users.noreply.github.com>
2024-11-18 18:55:51 +0500

    Add files via upload

--- sixth_task.py
+++ sixth_task.py
@@ -58,29 +58,24 @@ data = json.loads(json_data)
 soup = BeautifulSoup("<html><head><title>JSON Data</title></head><body></body></html>", "html.parser")
 body = soup.body
 
-# Шаг 3: Преобразование данных
 for item in data:
     item_div = soup.new_tag("div", **{"class": "item"})
 
-    # Добавляем ID
     id_tag = soup.new_tag("p")
     id_tag.string = f"ID: {item['id']}"
     item_div.append(id_tag)
 
-    # Добавляем категорию (если есть)
     if "category" in item:
         category_tag = soup.new_tag("p")
         category_name = item["category"]["name"]
         category_tag.string = f"Category: {category_name}"
         item_div.append(category_tag)
 
-    # Добавляем имя
     if "name" in item:
         name_tag = soup.new_tag("p")
         name_tag.string = f"Name: {item['name']}"
         item_div.append(name_tag)
 
-    # Добавляем фото
     photo_urls = item.get("photoUrls", [])
     if photo_urls:
         photo_div = soup.new_tag("div", **{"class": "photos"})
@@ -90,7 +85,6 @@ for item in data:
             photo_div.append(photo_tag)
         item_div.append(photo_div)
 
-    # Добавляем теги
     tags = item.get("tags", [])
     if tags:
         tags_div = soup.new_tag("div", **{"class": "tags"})
@@ -100,14 +94,11 @@ for item in data:
             tags_div.append(tag_p)
         item_div.append(tags_div)
 
-    # Добавляем статус
     status_tag = soup.new_tag("p")
     status_tag.string = f"Status: {item['status']}"
     item_div.append(status_tag)
 
-    # Добавляем в тело документа
     body.append(item_div)
 
-# Шаг 4: Сохранение HTML в файл
 with open("result_sixth_task.html", "w", encoding="utf-8") as file:
     file.write(soup.prettify())
